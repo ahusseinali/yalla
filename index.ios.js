600a4a7915ba4ef7dd1e239f22ae7d030a1f0f5f
Ahmed Ali <a.husseinzaki@gmail.com>
2016-09-19 16:52:50 -0700

    feat: Migrate changes to iOS

--- index.ios.js
+++ index.ios.js
@@ -4,116 +4,50 @@
  * @flow
  */
 
-var events = require('./data.js');
+import EventListScreen from './screens/event-list';
+import EventDetailsScreen from './screens/event-details';
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
-  Image,
-  View,
-  ListView
+  Navigator
 } from 'react-native';
 
 class YallaApp extends Component {
-  constructor() {
-    super();
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-      dataSource: ds.cloneWithRows(events.data),
-    };
-  }
-
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Hello Ahmed!</Text>
-        <ListView style={styles.eventList}
-          dataSource={this.state.dataSource}
-          renderRow={(event) => {return this._renderEventBrief(event)}} />
-      </View>
+    return(
+      <Navigator
+        initialRoute={{ident: "EventList"}}
+        ref="appNavigator"
+        style={styles.navigatorStyle}
+        renderScene={this._renderScene} />
     );
   }
 
-  _renderEventBrief(event) {
-    return (
-      <View style={styles.itemContainer}>
-        <View style={styles.itemDescription}>
-          <Text style={styles.itemTitle}>{event.title}</Text>
-          <Text style={styles.itemOwner}>By: {event.owner}</Text>
-          <Text style={styles.itemExpiry}>{event.expiry}</Text>
-        </View>
-        <View style={styles.itemParticipants}>
-          <Image source={{uri: event.pic}} style={styles.itemPicture}/>
-          <Text style={styles.itemParticipantsCount}>+{event.attendants.length}</Text>
-        </View>
-      </View>
-    );
+  _renderScene(route, navigator) {
+    var globalNavigatorProps = { navigator };
+
+    switch(route.ident) {
+      case "EventList":
+        return(
+          <EventListScreen {...globalNavigatorProps} />
+        )
+      case "EventDetails":
+        return(
+          <EventDetailsScreen
+          {...globalNavigatorProps}
+          event={route.event} />
+        )
+      default:
+        return(
+          <EventListScreen {...globalNavigatorProps} />
+        )
+    }
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    paddingTop: 10
-  },
-
-  welcome: {
-    fontSize: 30,
-    paddingLeft: 30
-  },
-
-  eventList: {
-    paddingTop: 10
-  },
-
-  itemContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'stretch',
-    backgroundColor: '#ccc',
-    paddingLeft: 20,
-    margin:5,
-    borderRadius: 5,
-    height: 110
-  },
-
-  itemDescription: {
-    flex: 1,
-    alignItems: 'flex-start', // horizontally align items
-    justifyContent: 'center'  //vertically align items
-  },
-
-  itemTitle: {
-    fontSize: 20,
-    fontWeight: 'bold'
-  },
-
-  itemOwner: {
-    fontSize: 16
-  },
-
-  itemExpiry: {
-    fontSize: 16,
-    fontStyle: 'italic'
-  },
-
-  itemParticipants: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-
-  itemPicture: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    margin: 10
-  },
-
-  itemParticipantsCount: {
-    fontSize: 20,
-  }
+  navigatorStyle: {}
 });
 
 AppRegistry.registerComponent('YallaApp', () => YallaApp);
