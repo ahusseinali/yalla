bcad32353cd890bac9f31f6546de9fad5851e48d
Ahmed Ali <a.husseinzaki@gmail.com>
2016-09-19 01:09:33 -0700

    feat: Port Android demo to iOS

--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,101 @@
  * @flow
  */
 
+var events = require('./data.js');
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
+  Image,
   View
 } from 'react-native';
+
+class EventBriefView extends Component {
+  render() {
+    console.log(this.props.event);
+    return (
+      <View style={styles.itemContainer}>
+        <View style={styles.itemDescription}>
+          <Text style={styles.itemTitle}>{this.props.event.title}</Text>
+          <Text style={styles.itemOwner}>By: {this.props.event.owner}</Text>
+          <Text style={styles.itemExpiry}>{this.props.event.expiry}</Text>
+        </View>
+        <View style={styles.itemParticipants}>
+          <Image source={{uri: this.props.event.pic}} style={styles.itemPicture}/>
+          <Text style={styles.itemParticipantsCount}>+{this.props.event.attendants.length}</Text>
+        </View>
+      </View>
+    );
+  }
+}
 
 class YallaApp extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={styles.eventList}>
+        <EventBriefView event={events.data[0]}></EventBriefView>
+        <EventBriefView event={events.data[1]}></EventBriefView>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  eventList: {
+    flex: 0,
+    alignItems: 'flex-start',
+    justifyContent: 'center'
+  },
+
+  itemContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    backgroundColor: '#ccc',
+    paddingLeft: 20,
+    margin:5,
+    borderRadius: 5,
+    height: 110
   },
-  welcome: {
+
+  itemDescription: {
+    flex: 1,
+    alignItems: 'flex-start', // horizontally align items
+    justifyContent: 'center'  //vertically align items
+  },
+
+  itemTitle: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    fontWeight: 'bold'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+
+  itemOwner: {
+    fontSize: 16
   },
+
+  itemExpiry: {
+    fontSize: 16,
+    fontStyle: 'italic'
+  },
+
+  itemParticipants: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  itemPicture: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    margin: 10
+  },
+
+  itemParticipantsCount: {
+    fontSize: 20,
+  }
 });
 
 AppRegistry.registerComponent('YallaApp', () => YallaApp);
